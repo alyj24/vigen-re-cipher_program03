6a6f0492a2beb34b77ace0fd4dbc7776b3fb96ad
Family <albon.136546120855@2depedqc.ph>
2023-04-21 00:26:23 +0800

    Pseudocodes

---
+++ the vigenere cipher.py
@@ -0,0 +1,12 @@
+print("Problem 03 ~ Assignment 2".center(42, "="))
+
+# pseudocode
+# message: THISISTHELASTTASKHOORDAY 19 7 8 18 8 18 19 7 4 11 0 18 10 7 14 14 17 3 0 24 
+# key: KNIGHTS                      10 13 8 6 7 19 18
+# add: 29 20 16 24 15 37 37 7 4 11 0 18 19 19 0 18 10 7 14 14 17 3 0 24
+# mod: 3 20 16 24 15 11 11 7 4 11 0 18 19 19 0 18 10 7 14 14 17 3 0 24 
+# ciphertext: D U Q Y P L L H E L A S T T A S K H O O R D A Y
+# define functions
+# ask the user for an input and save its input
+# recognize the input by the user
+# print the output
