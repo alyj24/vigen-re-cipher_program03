b64e858694a4c5616f2ccb41c04bf5cc4457d21e
Family <albon.136546120855@2depedqc.ph>
2023-04-21 00:32:58 +0800

    First Function is Defined.

--- the vigenere cipher.py
+++ the vigenere cipher.py
@@ -7,6 +7,16 @@ print("Problem 03 ~ Assignment 2".center(42, "="))
 # mod: 3 20 16 24 15 11 11 7 4 11 0 18 19 19 0 18 10 7 14 14 17 3 0 24 
 # ciphertext: D U Q Y P L L H E L A S T T A S K H O O R D A Y
 # define functions
+def _pad_key(plaintext, key):
+    padded_key = ""
+    i = 0
+    for char in plaintext:
+        if char.isalpha():
+            padded_key += key[i % len(key)]
+        i += 1                      
+    else:
+            padded_key += ""
+    return padded_key        
 # ask the user for an input and save its input
 # recognize the input by the user
 # print the output
